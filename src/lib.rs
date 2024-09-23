f75fb8fa4b286254001a8596e9e65ab0eb666d58
kimuti-tsukai <ruster354@gmail.com>
2024-09-23 16:15:36 +0900

    Impl global caching

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,7 @@
 use proc_macro::TokenStream;
+use proc_macro2::Span;
 use quote::quote;
-use syn::{parse_macro_input, Block, FnArg, ItemFn, ReturnType, Type};
+use syn::{parse_macro_input, Block, FnArg, Ident, ItemFn, ReturnType, Type};
 
 #[proc_macro_attribute]
 pub fn cacher(_args: TokenStream, input: TokenStream) -> TokenStream {
@@ -82,6 +83,86 @@ pub fn cacher(_args: TokenStream, input: TokenStream) -> TokenStream {
     input.block = Box::new(new_block);
 
     quote! {
+        #input
+    }
+    .into()
+}
+
+#[proc_macro_attribute]
+pub fn global_cacher(_args: TokenStream, input: TokenStream) -> TokenStream {
+    let mut input = parse_macro_input!(input as ItemFn);
+
+    let sig = &input.sig;
+
+    let inputs = &sig.inputs;
+
+    let mut tuple_args = Vec::new();
+    let mut args_ident = Vec::new();
+
+    for i in inputs {
+        if let FnArg::Typed(ty) = i {
+            tuple_args.push(ty.ty.to_owned());
+            args_ident.push(ty.pat.to_owned());
+        }
+    }
+
+    let args_type = quote! {
+        (#(#tuple_args),*)
+    }
+    .into();
+
+    let args_type = parse_macro_input!(args_type as Type);
+
+    let return_type = *(match &input.sig.output {
+        ReturnType::Default => {
+            let temp = quote! { () }.into();
+            Box::new(parse_macro_input!(temp as Type))
+        }
+        ReturnType::Type(_, t) => t.to_owned(),
+    });
+
+    let fn_ident = input.sig.ident.clone().to_string();
+
+    let constructor_ident_string = String::from("_constructor_") + &fn_ident;
+
+    let constructor_ident = Ident::new(&constructor_ident_string, Span::call_site());
+
+    let database_ident_string = String::from("_DATA_BASE_") + &fn_ident;
+
+    let database_ident = Ident::new(&database_ident_string, Span::call_site());
+
+    let inside_block = &input.block;
+
+    let inside_block = quote! {
+        {
+            if let Some(result) = #database_ident.lock().unwrap().get(&(#(#args_ident),*)) {
+                return result.to_owned()
+            }
+
+            let result = (|| #inside_block)();
+
+            #database_ident.lock().unwrap().insert((#(#args_ident),*), result.clone());
+
+            result
+        }
+    }
+    .into();
+
+    let inside_block = parse_macro_input!(inside_block as Block);
+
+    input.block = Box::new(inside_block);
+
+    quote! {
+        #[allow(non_snake_case)]
+        fn #constructor_ident() -> std::sync::Mutex<std::collections::HashMap<#args_type, #return_type>> {
+            std::sync::Mutex::new(
+                std::collections::HashMap::new()
+            )
+        }
+        #[allow(non_upper_case_globals)]
+        static #database_ident: std::sync::LazyLock<std::sync::Mutex<std::collections::HashMap<#args_type, #return_type>>> =
+            std::sync::LazyLock::new(#constructor_ident);
+
         #input
     }
     .into()
--- tests/cache_test.rs
+++ tests/cache_test.rs
@@ -10,5 +10,29 @@ fn fact(i: i32) -> i32 {
 
 #[test]
 fn fact_test() {
-    fact(5);
+    dbg!(fact(5));
+}
+
+#[cache_rec::global_cacher]
+fn fact_global(i: i32) -> i32 {
+    if i == 0 {
+        1
+    } else {
+        i * fact(i-1)
+    }
+}
+
+#[cache_rec::global_cacher]
+fn fact_global2(i: i32) -> i32 {
+    if i == 0 {
+        1
+    } else {
+        i * fact(i-1)
+    }
+}
+
+#[test]
+fn fact_global_test() {
+    dbg!(fact_global(8));
+    dbg!(fact_global2(3));
 }
